ba793132e8f525ca62fc59fe2425c0396af2ea87
elifnur03 <elifnurturut999@gmail.com>
2026-03-14 18:32:48 +0300

    Java Maps

---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,34 @@
+package org.example.entity;
+
+public class Employee {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+
+    public Employee(int id, String firstname, String lastname) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
